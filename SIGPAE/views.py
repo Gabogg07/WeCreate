6c9ec7e894534c3c47d9fd8d5c3a4da9f5888b5f
Julio <jfuenmayor96@gmail.com>
2017-02-16 18:26:04 -0400

    agregada lib textract a SIGPAE

--- SIGPAE/views.py
+++ SIGPAE/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+import textract
+
 def index(request):
-	texto_editable = "Este es el texto que quiero editar"
+	#texto_editable = "Este es el texto que quiero editar"
+	texto_editable = textract.process('/home/julio/prueba.pdf')
+
 	context = {'texto_editable': texto_editable,}
 	return render(request, 'SIGPAE/index.html', context)
