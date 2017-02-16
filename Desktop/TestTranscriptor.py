7ae694d6c58dc1b01e494a9ddbf88f45367830c8
Gabogg07 <gabrieleduardogg@gmail.com>
2017-02-16 18:06:30 -0400

    Prueba con Archivo Invalido

--- Desktop/TestTranscriptor.py
+++ Desktop/TestTranscriptor.py
@@ -5,8 +5,13 @@ from transcriptor import Transcriptor
 class Test(unittest.TestCase):
 
 	def testTranscriptor(self):
-		T=Transcriptor('MA12112.pdf','Salida')
+		T=Transcriptor('MA1112.pdf','Salida')
 		self.assertNotEqual(T.f,'Error',"Error en la transcripcion")
 	
-	def testÑ(self):
-		pass
+	def testMalicioso(self):
+		#Se prueba con un archivo que no existe
+		T=Transcriptor('QWERTY','Salida')
+		self.assertEqual(T.f,'Error',"Error en apertura de archivo")
+		
+			
+		
