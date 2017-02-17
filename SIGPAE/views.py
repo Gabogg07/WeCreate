44f30497ff33f0d172558bca5e78924183a06bfc
Giannimanilia <giannimanilia@gmail.com>
2017-02-16 20:39:43 -0400

    Carga de archivo
    - Se permite cargar un archivo buscándolo en la computadora
    - Al buscar el archivo se procesa automáticamente

--- SIGPAE/views.py
+++ SIGPAE/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 import textract
 
-def index(request):
-	#texto_editable = "Este es el texto que quiero editar"
-	texto_editable = textract.process('/home/julio/prueba.pdf')
+from .models import Document
+from .forms import DocumentForm
 
-	context = {'texto_editable': texto_editable,}
+def index(request):
+	context = {}
 	return render(request, 'SIGPAE/index.html', context)
+
+def upload_file(request):
+	if request.method == 'POST':
+		form = DocumentForm(request.POST, request.FILES)
+		if form.is_valid():
+			newdoc = Document(docfile=request.FILES['docfile'])
+			newdoc.save()
+
+			texto_editable = textract.process(newdoc.docfile.url)
+			context = {'texto_editable': texto_editable,}
+			return render(request, 'SIGPAE/index.html', context)
+	else:
+		form = DocumentForm()
+
+	documents = Document.objects.all()
+
+	return render(request,'SIGPAE/upload.html',{'documents': documents, 'form': form})
